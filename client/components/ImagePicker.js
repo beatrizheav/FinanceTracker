59dc7958e4fff9964095db2a390a1824ba27283c
Beatriz Avila <beatriz.heav@gmail.com>
2025-03-14 16:53:27 -0600

    feature: take picture and showed

--- client/components/ImagePicker.js
+++ client/components/ImagePicker.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { CameraView, useCameraPermissions } from "expo-camera";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
 
 const ImagePickerComponent = () => {
   const [facing, setFacing] = useState("back");
   const [permission, requestPermission] = useCameraPermissions();
+  const [photo, setPhoto] = useState(null);
 
   // Handle permission loading
   if (!permission) return <View />;
@@ -26,15 +34,51 @@ const ImagePickerComponent = () => {
     setFacing((prev) => (prev === "back" ? "front" : "back"));
   };
 
+  // Capture photo
+  const takePicture = async (camera) => {
+    if (camera) {
+      const photo = await camera.takePictureAsync();
+      setPhoto(photo.uri);
+    }
+  };
+
+  const takeNewPhoto = () => {
+    setPhoto(null);
+  };
+
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
+      {photo ? (
+        <View style={styles.container}>
+          <Image source={{ uri: photo }} style={styles.preview} />
+          <TouchableOpacity style={styles.button} onPress={takeNewPhoto}>
+            <Text style={styles.text}>Take a New Photo</Text>
           </TouchableOpacity>
         </View>
-      </CameraView>
+      ) : (
+        <CameraView
+          style={styles.camera}
+          facing={facing}
+          ref={(ref) => {
+            this.camera = ref;
+          }}
+        >
+          <View style={styles.overlay}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={toggleCameraFacing}
+            >
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => takePicture(this.camera)}
+            >
+              <Text style={styles.text}>Take Picture</Text>
+            </TouchableOpacity>
+          </View>
+        </CameraView>
+      )}
     </View>
   );
 };
@@ -50,22 +94,32 @@ const styles = StyleSheet.create({
     textAlign: "center",
     paddingBottom: 10,
   },
+  cameraContainer: {
+    flex: 1,
+  },
   camera: {
     flex: 1,
   },
-  buttonContainer: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    backgroundColor: "transparent",
+  overlay: {
+    position: "absolute",
+    bottom: 50,
+    left: 0,
+    right: 0,
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
   button: {
     padding: 10,
-    backgroundColor: "rgba(0,0,0,0.5)",
+    backgroundColor: "rgba(0,0,0,0.7)",
     borderRadius: 10,
   },
   text: {
     color: "white",
     fontSize: 18,
+  },
+  preview: {
+    height: "80%",
+    width: "80%",
+    borderRadius: 10,
   },
 });
