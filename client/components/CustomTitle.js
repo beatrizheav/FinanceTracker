0b8ae8ba5874cb26a3ad891e89af7faf2ab11784
Diana Campos <dcampos0495@gmail.com>
2025-03-18 20:53:01 -0600

    enhancement: allow color customization in Title and Text Component

--- client/components/CustomTitle.js
+++ client/components/CustomTitle.js
@@ -1,13 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { fontsTheme } from '../styles/fontsTheme'
+import { colorsTheme } from '../styles/colorsTheme';
 
-const CustomTitle = ({title, type, numberOfLines = 1}) => {
-    
+const CustomTitle = ({title, type, numberOfLines = 1, color}) => {
+  const textColor = color ? color : colorsTheme.black;
     return (
           <View>
               <Text 
-              style={[fontsTheme[type]]}
+              style={[fontsTheme[type], textColor]}
               numberOfLines={numberOfLines} 
               ellipsizeMode="tail">{title}</Text>
           </View>
