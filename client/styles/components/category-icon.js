051e18c24e8dbb8b754f55cdf08d244f9f325c36
Beatriz Avila <beatriz.heav@gmail.com>
2025-03-21 12:39:34 -0600

    refactor: categoryIcon component created for reusability

---
+++ client/styles/components/category-icon.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const categoryIcon = StyleSheet.create({
+  container: {
+    borderRadius: 50,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  big: {
+    width: 80,
+    height: 80,
+  },
+  small: { width: 50, height: 50 },
+});
