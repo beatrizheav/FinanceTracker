63ad055dc99f19b5d80bc692b1fe0a57e247681e
Diana Campos <dcampos0495@gmail.com>
2025-03-26 12:39:13 -0600

    feat: create Modal Income Component wit styles and example data

---
+++ client/constants/income.js
@@ -0,0 +1,12 @@
+export const income = {
+    "userId": 1,
+    "incomeId": 1,
+    "name": "Pago de renta",
+    "date": "2011-10-10T14:48:00",
+    "color": "#3b6e40",
+    "icon": {
+        "iconName": "attach-money",
+        "iconSet": "MaterialIcons"
+        },
+    "quantity": 1000,
+  }
