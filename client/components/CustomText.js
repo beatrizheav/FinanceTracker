e2ac88e6da3f43079c9e9d1543c82ba18bdcbbe6
Diana Campos <dcampos0495@gmail.com>
2025-04-01 12:13:53 -0600

    feat: change quantity colors in Activity Display and change Custom Text

--- client/components/CustomText.js
+++ client/components/CustomText.js
@@ -3,13 +3,12 @@ import React from "react";
 import { fontsTheme } from "../styles/fontsTheme";
 import { colorsTheme } from "../styles/colorsTheme";
 
-const CustomText = ({ text, type, numberOfLines = 1, color }) => {
+const CustomText = ({ text, type, numberOfLines = 1, color = colorsTheme.black }) => {
   const textStyle = type ? type : "TextBig";
-  const textColor = color ? color : colorsTheme.black;
   return (
     <View>
       <Text
-        style={[fontsTheme[textStyle], { color: textColor }]}
+        style={[fontsTheme[textStyle], {color: color} ]}
         numberOfLines={numberOfLines}
         ellipsizeMode="tail"
       >
