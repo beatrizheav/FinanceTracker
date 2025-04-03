030b2dc15b6afd1c7d642997429464454f07874e
Beatriz Avila <beatriz.heav@gmail.com>
2025-04-03 11:24:39 -0600

    style: delete recent activity style

--- client/styles/screens/home.js
+++ client/styles/screens/home.js
@@ -1,10 +1,4 @@
 import { StyleSheet } from "react-native";
 export const homeStyles = StyleSheet.create({
   expensesScrollview: { marginHorizontal: -16, paddingVertical: 10 },
-  headerRecentActivity: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingVertical: 8,
-  },
 });
