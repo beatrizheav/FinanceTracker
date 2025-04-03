eeb11f16c516b319ccafca9ad13afc24c81018df
ljau <ljau07@gmail.com>
2025-04-03 11:48:34 -0600

    feat: Create categories screen

--- client/constants/categories.js
+++ client/constants/categories.js
@@ -1,4 +1,4 @@
-export const categories = [
+export const categoriesData = [
   {
     name: "Alimentación",
     description: "Gastos relacionados con comida y bebidas.",
@@ -7,6 +7,74 @@ export const categories = [
     color: "#FF6347",
     icon: {
       iconName: "fast-food-outline",
+      iconSet: "Ionicons",
+    },
+  },
+  {
+    name: "Transporte",
+    description: "Gastos relacionados con transporte público, gasolina, etc.",
+    budget: 200,
+    totalExpenses: 150,
+    color: "#4682B4",
+    icon: {
+      iconName: "car-sport-outline",
+      iconSet: "Ionicons",
+    },
+  },
+  {
+    name: "Entretenimiento",
+    description: "Gastos de actividades recreativas y de ocio.",
+    budget: 300,
+    totalExpenses: 100,
+    color: "#FFD700",
+    icon: {
+      iconName: "game-controller-outline",
+      iconSet: "Ionicons",
+    },
+  },
+  {
+    name: "Salud",
+    description: "Gastos en consultas médicas, medicamentos y tratamientos.",
+    budget: 400,
+    totalExpenses: 200,
+    color: "#32CD32",
+    icon: {
+      iconName: "medkit-outline",
+      iconSet: "Ionicons",
+    },
+  },
+  {
+    name: "Vivienda",
+    description:
+      "Gastos relacionados con el alquiler, hipoteca y mantenimiento.",
+    budget: 1000,
+    totalExpenses: 800,
+    color: "#FBBC04",
+    icon: {
+      iconName: "home-outline",
+      iconSet: "Ionicons",
+    },
+  },
+  {
+    name: "Educación",
+    description:
+      "Gastos relacionados con matrícula, libros y materiales educativos.",
+    budget: 600,
+    totalExpenses: 400,
+    color: "#1E90FF",
+    icon: {
+      iconName: "book-outline",
+      iconSet: "Ionicons",
+    },
+  },
+  {
+    name: "Ropa",
+    description: "Gastos en prendas de vestir y accesorios.",
+    budget: 150,
+    totalExpenses: 120,
+    color: "#D2691E",
+    icon: {
+      iconName: "shirt-outline",
       iconSet: "Ionicons",
     },
   },
---
+++ client/styles/screens/categories.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const categoriesScreen = StyleSheet.create({
+  container: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    paddingBottom: 200,
+  },
+  listContainer: {
+    marginTop: 50,
+  },
+});
