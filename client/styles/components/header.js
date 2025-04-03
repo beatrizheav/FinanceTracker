c298a94e48288515fd51e7e394fa16292330f01f
Beatriz Avila <beatriz.heav@gmail.com>
2025-04-03 11:16:34 -0600

    chore: delete unnecesary imports

--- client/styles/components/header.js
+++ client/styles/components/header.js
@@ -1,4 +1,4 @@
-import { Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { colorsTheme } from "../colorsTheme";
 
 export const header = StyleSheet.create({
