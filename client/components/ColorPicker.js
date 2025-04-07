3caf1c2179e503ccb577dd78c6ec115cc716f0f3
Beatriz Avila <beatriz.heav@gmail.com>
2025-04-07 10:31:53 -0600

    style: Texts tags replaced with CustomText

--- client/components/ColorPicker.js
+++ client/components/ColorPicker.js
@@ -1,18 +1,12 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  ActivityIndicator,
-  TouchableOpacity,
-  Modal,
-} from "react-native";
+import React from "react";
+import { View, ActivityIndicator, TouchableOpacity, Modal } from "react-native";
 import ColorPicker from "react-native-wheel-color-picker";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import CustomButton from "./CustomButton";
+import CustomText from "./CustomText";
 import { inputs } from "../styles/components/inputs";
 import { colorPicker } from "../styles/components/color-picker";
-import { fontsTheme } from "../styles/fontsTheme";
 import { colorsTheme } from "../styles/colorsTheme";
-import CustomButton from "./CustomButton";
 
 const ColorPickerModal = ({
   show,
@@ -68,7 +62,7 @@ const ColorPickerComponent = ({ color, setColor, show, setShow }) => {
 
   return (
     <View style={inputs.wrapper}>
-      <Text style={fontsTheme.TitleSmall}>Selecciona un color</Text>
+      <CustomText type={"TitleSmall"} text={"Selecciona un color"} />
       <TouchableOpacity
         style={[inputs.container, inputs.directionRow, inputs.containerSquare]}
         onPress={() => setShow(!show)}
