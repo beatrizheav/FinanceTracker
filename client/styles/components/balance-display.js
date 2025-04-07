4ee4020cca565bb1cca0975f0536588b18ce2786
Beatriz Avila <beatriz.heav@gmail.com>
2025-04-07 11:00:44 -0600

    Refactor: Text tags replaced with CustomText

--- client/styles/components/balance-display.js
+++ client/styles/components/balance-display.js
@@ -2,22 +2,19 @@ import { StyleSheet } from "react-native";
 import { colorsTheme } from "../colorsTheme";
 
 export const balanceDisplay = StyleSheet.create({
-    container: {
-        borderRadius: 20,
-        padding: 15,
-    },
-    details: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginVertical: 3,
-    },
-    amount: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: 120,
-        alignItems: 'center',
-    },
-    white: {
-        color: colorsTheme.white
-    }
-})
+  container: {
+    borderRadius: 20,
+    padding: 15,
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 3,
+  },
+  amount: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: 120,
+    alignItems: "center",
+  },
+});
