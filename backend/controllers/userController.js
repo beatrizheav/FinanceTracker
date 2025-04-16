c0284ae8a1a18bc6e9a4e0c98380b0b82658e756
Diana Campos <dcampos0495@gmail.com>
2025-04-16 13:14:02 -0600

    feat: create loginUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const db = require("../config/db");
+const bcrypt = require("bcrypt")
 
 const getAllUsers = (req, res) => {
   db.query("SELECT * FROM users", (err, results) => {
@@ -12,4 +13,44 @@ const getAllUsers = (req, res) => {
   });
 };
 
-module.exports = { getAllUsers };
+const loginUser = (req, res) => {
+  const {email, password} = req.body;
+
+  if(!email || !password){
+    return res
+      .status(400)
+      .json({message: "Email and password are required"});
+  }
+
+  db.execute("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
+    if(err){
+      console.error(err);
+      return res.status(500).json({message: "Interval server error"});
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({message: "User not found"});
+    };
+
+    const user = results[0]
+    bcrypt.compare(password, user.password)
+    .then((passwordMatch) => {
+      if(passwordMatch){
+        res.status(200).json({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          avatar: user.avatar,
+        })
+      }else{
+        return res.status(400).json({message: "Invalid email or password"})
+      }
+    })
+    .catch((compareErr) => {
+        console.error(compareErr);
+        return res.status(500).json({ message: "Error validating password" });
+    })
+  })
+}
+
+module.exports = { getAllUsers, loginUser };
