e4f9b768b176fe001510f0d6833ecb775537cda5
ljau <ljau07@gmail.com>
2025-04-16 12:56:43 -0600

    feat: Created add income endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const userController = require("./controllers/userController");
+const incomeController = require("./controllers/incomeController");
 
 require("dotenv").config();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 
 // Routes Users
 app.get("/users", userController.getAllUsers);
+app.post("/incomes/add", incomeController.createIncome);
+app.get("/incomes", incomeController.getAllIncomes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
