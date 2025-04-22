b9c6c93bb3603fd863e99350db6eb339a3f1c581
Diana Campos <dcampos0495@gmail.com>
2025-04-22 13:02:54 -0600

    feat: add create category functionality on backend

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,12 @@ app.get(
   categoriesController.getUserCategories
 );
 
+app.post(
+  "/category/add",
+  authenticate,
+  categoriesController.createCategory
+);
+
 const PORT = process.env.PORT || 5001;
 app.listen(5001, '0.0.0.0',() => {
   console.log(`Server is running on port ${PORT}`);
