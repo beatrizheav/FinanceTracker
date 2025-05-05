fdf091f97b102dfcf3eb3194e3593dd06eb75a05
ljau <ljau07@gmail.com>
2025-05-05 12:14:26 -0600

    feat: Integrate user incomes to front end

--- client/components/ActivityDisplay.js
+++ client/components/ActivityDisplay.js
@@ -1,74 +1,91 @@
-import { TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { TouchableOpacity, View } from "react-native";
+import React from "react";
 import { Ionicons } from "@expo/vector-icons";
-import { format } from 'date-fns';
-import { es } from 'date-fns/locale';
-import { activityDisplay } from '../styles/components/activity-display'
-import CustomText from './CustomText'
-import { colorsTheme } from '../styles/colorsTheme';
-import CategoryIcon from './CategoryIcon';
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+import { activityDisplay } from "../styles/components/activity-display";
+import CustomText from "./CustomText";
+import { colorsTheme } from "../styles/colorsTheme";
+import CategoryIcon from "./CategoryIcon";
 
 const ActivityDisplay = ({
-    name, 
-    date, 
-    quantity = 0,
-    color, 
-    onPress, 
-    category = {}, 
-    screen}) => {
-    const formatName = name ? name : 'nombre no encontrado'; //Validates if there is data in the title, if not, sets a default title
-    const formatDate = date ? format(date, "dd 'de' MMMM yyyy", {locale: es}) : 'fecha no encontrada'; //takes the date and formats it
-    
-    const icon = screen === 'income' 
-        ?  {"iconName": "attach-money","iconSet": "MaterialIcons"} 
-        : screen === 'category'
-            ? category
-            : category?.icon;
-    
-    const colorIcon = screen === 'income' 
-        ? activityDisplay.green.color 
-        : screen === 'category'
-            ? color
-            : category.color;
-    
-    const quantityColor = screen === 'income' //changes the text color depending on the screen
-        ? activityDisplay.green.color
-        : screen === 'expense'
-            ? activityDisplay.red.color
-            : activityDisplay.teal.color
-    
-            const validQuantity = Number(quantity) || 0; //Validates if the quantity is a number and if not, adds 0 by default.
-    
-    const quantityText = quantity 
-        ? `${screen === 'expense' ? '-' : ''} $ ${validQuantity.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}` 
-        : '$ 0.00'; //Validates if there is data in the quantity, if not, sets a default quantity
-  return (
-    <TouchableOpacity 
-        onPress={onPress}
-        style={[activityDisplay.container, activityDisplay.format]}>
-        <View style={activityDisplay.format}>      
-            <CategoryIcon icon={icon} type={"small"} color={colorIcon}/>
-            <View style={activityDisplay.description}>
-                <CustomText text={formatName} type={'TextBig'}/>
-                { screen === 'category' ? null : <CustomText text={formatDate} type={'TextSmall'} color={activityDisplay.darkGray.color}/>}
-            </View>
-        </View>
-        <View style={activityDisplay.format}>
-            <CustomText 
-                text={quantityText} 
-                type={'TextBig'}
-                color={quantityColor}
-            />
-            <Ionicons 
-                testID="chevron-forward" 
-                name={'chevron-forward'} 
-                size={22} 
-                color={colorsTheme.dark} 
-                style={activityDisplay.iconForward}
-            />
-        </View>
-    </TouchableOpacity>
-  )
-}
+  name,
+  date,
+  amount,
+  fixed,
+  color,
+  onPress,
+  category = {},
+  screen,
+}) => {
+  const formatName = name ? name : "nombre no encontrado"; //Validates if there is data in the title, if not, sets a default title
+  const formatDate = date
+    ? format(date, "dd 'de' MMMM yyyy", { locale: es })
+    : "fecha no encontrada"; //takes the date and formats it
 
-export default ActivityDisplay
+  const icon =
+    screen === "income"
+      ? { iconName: "attach-money", iconSet: "MaterialIcons" }
+      : screen === "category"
+      ? category
+      : category?.icon;
+
+  const colorIcon =
+    screen === "income"
+      ? activityDisplay.green.color
+      : screen === "category"
+      ? color
+      : category.color;
+
+  const quantityColor =
+    screen === "income" //changes the text color depending on the screen
+      ? activityDisplay.green.color
+      : screen === "expense"
+      ? activityDisplay.red.color
+      : activityDisplay.teal.color;
+
+  const validQuantity = Number(amount) || 0; //Validates if the amount is a number and if not, adds 0 by default.
+
+  const quantityText = amount
+    ? `${screen === "expense" ? "-" : ""} $ ${validQuantity.toLocaleString(
+        "en-US",
+        { minimumFractionDigits: 2, maximumFractionDigits: 2 }
+      )}`
+    : "$ 0.00"; //Validates if there is data in the amount, if not, sets a default amount
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[activityDisplay.container, activityDisplay.format]}
+    >
+      <View style={activityDisplay.format}>
+        <CategoryIcon icon={icon} type={"small"} color={colorIcon} />
+        <View style={activityDisplay.description}>
+          <CustomText text={formatName} type={"TextBig"} />
+          {screen === "category" ? null : (
+            <CustomText
+              text={formatDate}
+              type={"TextSmall"}
+              color={activityDisplay.darkGray.color}
+            />
+          )}
+        </View>
+      </View>
+      <View style={activityDisplay.format}>
+        <CustomText
+          text={quantityText}
+          type={"TextBig"}
+          color={quantityColor}
+        />
+        <Ionicons
+          testID="chevron-forward"
+          name={"chevron-forward"}
+          size={22}
+          color={colorsTheme.dark}
+          style={activityDisplay.iconForward}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ActivityDisplay;
