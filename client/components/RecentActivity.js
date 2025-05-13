a8a56a87b343b03da0170e9ee4860cad5246e722
Diana Campos <dcampos0495@gmail.com>
2025-05-13 12:57:06 -0600

    enhance: add hook to RecentActivity component and add loader

--- client/components/RecentActivity.js
+++ client/components/RecentActivity.js
@@ -1,18 +1,15 @@
 import {
   View,
-  TouchableOpacity,
   FlatList,
-  ImageBackground,
 } from "react-native";
 import React, { useMemo } from "react";
-import { incomesData } from "../constants/incomesData";
-import { expensesData } from "../constants/expensesData";
+import useExpensesAndIncomes from "../hooks/useExpenseAndIncomes";
 import CustomText from "./CustomText";
 import ActivityDisplay from "./ActivityDisplay";
 import { recentActivity } from "../styles/components/recent-activity";
-import { colorsTheme } from "../styles/colorsTheme";
+import { homeStyles } from "../styles/screens/home";
 
-// Function to combine and sort data
+//Function to combine and sort data
 const mergeAndSortData = (incomes, expenses) => {
   return [...incomes, ...expenses]
     .map((item, index) => ({ ...item, id: (index + 1).toString() }))
@@ -21,8 +18,9 @@ const mergeAndSortData = (incomes, expenses) => {
 };
 
 export default function RecentActivity({ onPress }) {
+  const {expenses, incomes, loading} = useExpensesAndIncomes();
   const combinedData = useMemo(
-    () => mergeAndSortData(incomesData, expensesData),
+    () => mergeAndSortData(incomes, expenses),
     []
   );
 
@@ -31,18 +29,28 @@ export default function RecentActivity({ onPress }) {
       <View style={recentActivity.header}>
         <CustomText text="Actividad Reciente" type="TitleSmall" />
       </View>
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        data={combinedData}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <ActivityDisplay
-            {...item}
-            screen={item.category ? "expense" : "income"}
-            onPress={() => onPress(item)}
+      {loading 
+        ? (
+          <View style={homeStyles.recentActivityContainer}>
+            <CustomText 
+            text={"Cargando..."} 
+            type={"TextSmall"}
+            />
+          </View>
+        ): (
+          <FlatList
+          showsVerticalScrollIndicator={false}
+          data={combinedData}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <ActivityDisplay
+              {...item}
+              screen={item.category ? "expense" : "income"}
+              onPress={() => onPress(item)}
+            />
+          )}
           />
         )}
-      />
     </View>
   );
 }
--- client/styles/screens/home.js
+++ client/styles/screens/home.js
@@ -1,4 +1,9 @@
 import { StyleSheet } from "react-native";
 export const homeStyles = StyleSheet.create({
   expensesScrollview: { marginHorizontal: -16, paddingVertical: 10 },
+  recentActivityContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+  }
 });
